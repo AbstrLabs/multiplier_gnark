b0f4b96b532d1828c373d5abe229c48e6e45674d
myz <zmy3324@outlook.com>
2022-02-06 17:15:29 +0900

    test cbol to deserialize

--- main.go
+++ main.go
@@ -6,19 +6,21 @@ import (
 	"os"
 	"runtime"
 	"time"
+	// "bytes"
 
+    "github.com/fxamacker/cbor/v2"
 	"github.com/consensys/gnark-crypto/ecc"
 	"github.com/consensys/gnark/backend"
 	"github.com/consensys/gnark/backend/groth16"
 	"github.com/consensys/gnark/frontend"
 )
 
-const N int = 13000
+const N int = 200000
 
 type Circuit struct {
 	A frontend.Variable
 	B frontend.Variable
-	C frontend.Variable `gnark:",public"`
+// 	C frontend.Variable `gnark:",public"`
 }
 
 func (circuit *Circuit) Define(api frontend.API) error {
@@ -31,13 +33,13 @@ func (circuit *Circuit) Define(api frontend.API) error {
 //         api.AddCounter(start, temp)
 	}
 // 	api.Println(V[N-1])
+// 	api.AssertIsEqual(circuit.C, V[N-1])
 	end := api.Tag("end")
     api.AddCounter(start, end)
-	api.AssertIsEqual(circuit.C, V[N-1])
 	return nil
 }
 
-func setup() {
+func setup(serialType string) {
 	var circuit Circuit
 	r1cs, err := frontend.Compile(ecc.BN254, backend.GROTH16, &circuit)
 	if err != nil {
@@ -49,28 +51,20 @@ func setup() {
         fmt.Println(c)
     }
 
-	f, err := os.Create("circuit")
-	defer f.Close()
-	r1cs.WriteTo(f)
-
 	pk, vk, err := groth16.Setup(r1cs)
 	if err != nil {
 		panic("setup failed")
 	}
 
-	f2, err := os.Create("pk")
-	defer f2.Close()
-	pk.WriteRawTo(f2)
+	writeToFile(serialType, r1cs, pk, vk)
+}
 
-	f3, err := os.Create("vk")
-	defer f3.Close()
-	vk.WriteTo(f3)
-
+func prove(r1cs frontend.CompiledConstraintSystem, pk groth16.ProvingKey, vk groth16.VerifyingKey) {
 	var w Circuit
 	w.A = 666
 	w.B = 233
 // 	N = 13000
-	w.C = "6793544489128382459281307073994348887771341450407551491096489485513352038568"
+// 	w.C = "6793544489128382459281307073994348887771341450407551491096489485513352038568"
 // 	N = 13000000
 // 	w.C = "11627185319010103288157029572521291604665626512789572484375095826433360489871"
 
@@ -93,6 +87,31 @@ func setup() {
 	if err != nil {
 		panic("verification failed")
 	}
+	fmt.Println("prove succeeded")
+}
+
+func writeToFile(serialType string, r1cs frontend.CompiledConstraintSystem, pk groth16.ProvingKey, vk groth16.VerifyingKey) {
+	// var buf bytes.Buffer
+	f1, _ := os.Create("circuit")
+	defer f1.Close()
+	f2, _ := os.Create("pk")
+	defer f2.Close()
+	f3, _ := os.Create("vk")
+	defer f3.Close()
+	r1cs.WriteTo(f1)
+	if (serialType == "native") {
+		pk.WriteTo(f2)
+		vk.WriteTo(f3)
+	} else if (serialType == "raw") {
+		pk.WriteRawTo(f2)
+		vk.WriteRawTo(f3)
+	} else if (serialType == "cbol") {
+		pk.WriteRawTo(f2)
+		// enc := cbor.NewEncoder(f2)
+		// _ = enc.Encode(pk)
+		enc := cbor.NewEncoder(f3)
+		_ = enc.Encode(vk)
+	}
 }
 
 func timeTrack(start time.Time, name string) {
@@ -114,11 +133,11 @@ func bToMb(b uint64) uint64 {
 	return b / 1024 / 1024
 }
 
-func prove() {
+func decode(serialType string) {
 	t := time.Now()
 	cs := groth16.NewCS(ecc.BN254)
-	f, _ := os.Open("circuit")
-	cs.ReadFrom(f)
+	f1, _ := os.Open("circuit")
+	cs.ReadFrom(f1)
 	PrintMemUsage()
 	timeTrack(t, "deserialize circuit")
 	println(cs.GetNbConstraints())
@@ -126,13 +145,33 @@ func prove() {
 	t = time.Now()
 	pk := groth16.NewProvingKey(ecc.BN254)
 	f2, _ := os.Open("pk")
-	pk.ReadFrom(f2)
+	if (serialType == "native" || serialType == "raw") {
+		pk.ReadFrom(f2)
+	}  else if (serialType == "cbol") {
+		pk.ReadFrom(f2)
+		// dec := cbor.NewDecoder(f2)
+		// _ = dec.Decode(pk)
+	}
 	PrintMemUsage()
 	timeTrack(t, "deserialize pk")
 
+	t = time.Now()
+	vk := groth16.NewVerifyingKey(ecc.BN254)
+	f3, _ := os.Open("vk")
+	if (serialType == "native" || serialType == "raw") {
+		vk.ReadFrom(f3)
+	}  else if (serialType == "cbol") {
+		dec := cbor.NewDecoder(f3)
+		_ = dec.Decode(vk)
+	}
+	PrintMemUsage()
+	timeTrack(t, "deserialize vk")
+
+	prove(cs, pk, vk)
 }
 
 func main() {
-	setup()
-	prove()
+	serialType := "cbol"
+	setup(serialType)
+	decode(serialType)
 }
